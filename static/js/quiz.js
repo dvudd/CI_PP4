e71ed389f181d5c6765df805fe0bc3e387355738
David Eriksson <the.dvudd@gmail.com>
2024-03-13 11:36:52 +0000

    feat: adds option for touchscreen users to swipe to change cards in quiz

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -9,30 +9,60 @@ document.addEventListener('DOMContentLoaded', function() {
     function updateButtonStates() {
         // Handle the previous button state
         if (currentCardIndex <= 0) {
-            document.getElementById('prev-question-btn').classList.add('disabled');
+            document.getElementById('prev-question-btn').classList.add('visually-hidden');
         } else {
-            document.getElementById('prev-question-btn').classList.remove('disabled');
+            document.getElementById('prev-question-btn').classList.remove('visually-hidden');
         }
         
         // Handle the next button state
         if (currentCardIndex >= cardsData.length - 1) {
-            document.getElementById('next-question-btn').classList.add('disabled');
+            document.getElementById('next-question-btn').classList.add('visually-hidden');
         } else {
-            document.getElementById('next-question-btn').classList.remove('disabled');
+            document.getElementById('next-question-btn').classList.remove('visually-hidden');
         }
     }
+
+    // Touch
+    let touchStart = 0;
+    let touchEnd = 0;
+    const questionArea = document.getElementById('question-area');
+
+    function swipe() {
+        if (touchEnd < touchStart) {
+            // Swipe Left - Show Next Question
+            document.getElementById('next-question-btn').click();
+        }
+        if (touchEnd > touchStart) {
+            // Swipe Right - Show Previous Question
+            document.getElementById('prev-question-btn').click();
+        }
+    }
+
+    questionArea.addEventListener('touchstart', e => {
+        touchStart = e.changedTouches[0].screenX;
+    });
+
+    questionArea.addEventListener('touchend', e => {
+        touchEnd = e.changedTouches[0].screenX;
+        swipe();
+    });
 
     // Show the question
     function displayQuestion() {
         const card = cardsData[currentCardIndex];
-        document.getElementById('question-text').textContent = card.question;
         // Display the question image, if there is one
         const questionImage = document.getElementById('question-image');
         if (card.question_image) {
+            document.getElementById("question-image").classList.remove("visually-hidden");
             questionImage.src = card.question_image;
-            questionImage.style.display = '';
         } else {
-            questionImage.style.display = 'none';
+            document.getElementById("question-image").classList.add("visually-hidden");
+        }
+        if (card.question) {
+            document.getElementById("question-text").classList.remove("visually-hidden");
+            document.getElementById('question-text').textContent = card.question;
+        } else {
+            document.getElementById("question-text").classList.add("visually-hidden");
         }
         updateButtonStates();
     }
