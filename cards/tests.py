0c0d02f9567744b4d9e5163cf6af8eb608519450
David Eriksson <the.dvudd@gmail.com>
2024-03-24 20:41:32 +0000

    test: adds formstest for the cards app

--- cards/tests.py
+++ cards/tests.py
@@ -3,21 +3,22 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
-from .models import Subject, Deck, Card
 from PIL import Image
+from .models import Subject, Deck, Card
+from .forms import SubjectForm, DeckForm, CardForm
 
 class ModelsTest(TestCase):
     @classmethod
-    def setUpTestData(cls):
-        cls.user = User.objects.create_user(username='testuser', password='12345')
-        cls.subject = Subject.objects.create(name="Nostalgia", creator=cls.user)
-        cls.deck = Deck.objects.create(name="Cartoons", subject=cls.subject)
-        cls.large_image_path = os.path.join(settings.BASE_DIR, 'cards/tests/test_images/sample-large.jpg')
-        cls.small_image_path = os.path.join(settings.BASE_DIR, 'cards/tests/test_images/sample-small.jpg')
+    def setUpTestData(self):
+        self.user = User.objects.create_user(username='testuser@example.com', password='12345')
+        self.subject = Subject.objects.create(name="Nostalgia", creator=self.user)
+        self.deck = Deck.objects.create(name="Cartoons", subject=self.subject)
+        self.large_image_path = os.path.join(settings.BASE_DIR, 'cards/tests/test_images/sample-large.jpg')
+        self.small_image_path = os.path.join(settings.BASE_DIR, 'cards/tests/test_images/sample-small.jpg')
 
     def test_subject_creation(self):
         self.assertEqual(self.subject.name, "Nostalgia")
-        self.assertEqual(self.subject.creator.username, "testuser")
+        self.assertEqual(self.subject.creator.username, "testuser@example.com")
 
     def test_deck_creation(self):
         deck = Deck.objects.get(id=1)
@@ -56,3 +57,41 @@ class ModelsTest(TestCase):
         with Image.open(card.answer_image) as answer_img:
             self.assertEqual(answer_img.format, 'WEBP', "Small image was not converted to WEBP format.")
             self.assertEqual((answer_img.width, answer_img.height), (500, 500), "Small image was resized incorrectly.")
+
+class FormsTest(TestCase):
+    @classmethod
+    def setUpTestData(self):
+        self.user = User.objects.create_user(username='testuser@example.com', password='12345')
+        self.large_image_path = os.path.join(settings.BASE_DIR, 'cards/tests/test_images/sample-large.jpg')
+        self.small_image_path = os.path.join(settings.BASE_DIR, 'cards/tests/test_images/sample-small.jpg')
+
+    def test_subject_form_valid(self):
+        form_data = {'name': 'Monty Python'}
+        form = SubjectForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_deck_form_valid(self):
+        form_data = {'name': 'the Holy Grail', 'description': 'The bridge of death'}
+        form = DeckForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_card_form_valid_with_text(self):
+        form_data = {'question': 'What is your name?', 'answer': 'It is Arthur, King of the Britons.'}
+        form = CardForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_card_form_valid_with_image(self):
+        with open(self.large_image_path, 'rb') as large_img:
+            large_image = SimpleUploadedFile(name='sample-large.jpg', content=large_img.read(), content_type='image/jpeg')
+        
+        with open(self.small_image_path, 'rb') as small_img:
+            small_image = SimpleUploadedFile(name='sample-small.jpg', content=small_img.read(), content_type='image/jpeg')
+        
+        form_data = {'question': 'What is your quest?', 'question_image': large_image, 'answer': 'To seek the Holy Grail.', 'answer_image': small_image}
+        form = CardForm(data=form_data, files={'question_image': large_image, 'answer_image': small_image})
+        self.assertTrue(form.is_valid())
+
+    def test_card_form_invalid(self):
+        form_data = {}
+        form = CardForm(data=form_data)
+        self.assertFalse(form.is_valid())
