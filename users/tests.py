ded76bc23f83e309202ea8a46042c1da52bdf12c
David Eriksson <the.dvudd@gmail.com>
2024-03-25 10:18:26 +0000

    refactor: fixes users tests to match CI python linter

--- users/tests.py
+++ users/tests.py
@@ -8,15 +8,21 @@ from PIL import Image
 from .models import Profile
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
+
 class ModelsTest(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username='test@example.com', password='testpass123')
-        self.large_image_path = os.path.join(settings.BASE_DIR, 'cards/tests/test_images/sample-large.jpg')
+        self.user = User.objects.create_user(
+            username='test@example.com',
+            password='testpass123'
+        )
+        self.large_image_path = os.path.join(
+            settings.BASE_DIR,
+            'cards/tests/test_images/sample-large.jpg'
+        )
 
     def test_profile_creation(self):
         user_profile = self.user.profile
         self.assertIsNotNone(user_profile)
-
 
     def test_register_user(self):
         response = self.client.post(reverse('register'), data={
@@ -27,8 +33,12 @@ class ModelsTest(TestCase):
             'password2': 'newpassword123',
         })
         self.assertEqual(response.status_code, 302)
-        self.assertTrue(User.objects.filter(email='newuser@example.com').exists())
-        self.assertTrue(User.objects.filter(username='newuser@example.com').exists())
+        self.assertTrue(User.objects.filter(
+            email='newuser@example.com').exists()
+        )
+        self.assertTrue(User.objects.filter(
+            username='newuser@example.com').exists()
+        )
 
     def test_login_user(self):
         self.client.login(username='test@example.com', password='testpass123')
@@ -38,7 +48,11 @@ class ModelsTest(TestCase):
     def test_update_profile(self):
         self.client.login(username='test@example.com', password='testpass123')
         with open(self.large_image_path, 'rb') as large_img:
-            large_image = SimpleUploadedFile(name='sample-large.jpg', content=large_img.read(), content_type='image/jpeg')
+            large_image = SimpleUploadedFile(
+                name='sample-large.jpg',
+                content=large_img.read(),
+                content_type='image/jpeg'
+            )
 
         response = self.client.post(reverse('profile'), data={
             'email': 'updated@example.com',
@@ -47,25 +61,38 @@ class ModelsTest(TestCase):
             'image': large_image,
         }, follow=True)
 
-        # Reload the user and profile from the database to get the updated values
         self.user.refresh_from_db()
         profile = self.user.profile
         profile.refresh_from_db()
 
-        # Verify the email was updated
         self.assertEqual(self.user.email, 'updated@example.com')
 
         # Verify that the profile image was converted and resized
         with Image.open(profile.image) as profile_img:
-            self.assertEqual(profile_img.format, 'WEBP', "Large image was not converted to WEBP format.")
-            self.assertTrue(profile_img.width <= 300 and profile_img.height <= 300, "Large image was not resized correctly.")
+            self.assertEqual(
+                profile_img.format,
+                'WEBP',
+                "Large image was not converted to WEBP format."
+            )
+            self.assertTrue(
+                profile_img.width <= 300 and profile_img.height <= 300,
+                "Large image was not resized correctly."
+            )
+
 
 class FormsTest(TestCase):
     @classmethod
-    def setUpTestData(self):
-        self.user = User.objects.create_user(username='test@example.com', email='test@example.com', password='testpass123')
-        self.profile = Profile.objects.get(user=self.user)
-        self.large_image_path = os.path.join(settings.BASE_DIR, 'cards/tests/test_images/sample-large.jpg')
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
+            username='test@example.com',
+            email='test@example.com',
+            password='testpass123'
+        )
+        cls.profile = Profile.objects.get(user=cls.user)
+        cls.large_image_path = os.path.join(
+            settings.BASE_DIR,
+            'cards/tests/test_images/sample-large.jpg'
+        )
 
     def test_user_register_form_valid(self):
         form_data = {
@@ -113,23 +140,42 @@ class FormsTest(TestCase):
 
     def test_profile_update_form_valid(self):
         with open(self.large_image_path, 'rb') as image_file:
-            form_data = {'image': SimpleUploadedFile(name='sample-large.jpg', content=image_file.read(), content_type='image/jpeg')}
-        form = ProfileUpdateForm(data={}, files=form_data, instance=self.profile)
+            form_data = {'image': SimpleUploadedFile(
+                name='sample-large.jpg',
+                content=image_file.read(),
+                content_type='image/jpeg')
+            }
+        form = ProfileUpdateForm(
+            data={},
+            files=form_data,
+            instance=self.profile
+        )
         self.assertTrue(form.is_valid())
+
 
 class AuthenticationTest(TestCase):
     @classmethod
-    def setUpTestData(self):
-        self.user = User.objects.create_user(username='test@example.com', email='test@example.com', password='testpass123')
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
+            username='test@example.com',
+            email='test@example.com',
+            password='testpass123'
+        )
 
     def test_login_success(self):
-        response = self.client.post(reverse('login'), {'email': 'test@example.com', 'password': 'testpass123'})
+        response = self.client.post(
+            reverse('login'),
+            {'email': 'test@example.com', 'password': 'testpass123'}
+        )
         self.assertRedirects(response, reverse('cards-home'))
         self.assertTrue('_auth_user_id' in self.client.session)
-        
+
     def test_login_failure(self):
-        response = self.client.post(reverse('login'), {'email': 'test@example.com', 'password': 'wrongpassword'})
-        self.assertEqual(response.status_code, 200) 
+        response = self.client.post(
+            reverse('login'),
+            {'email': 'test@example.com', 'password': 'wrongpassword'}
+        )
+        self.assertEqual(response.status_code, 200)
         self.assertTrue('_auth_user_id' not in self.client.session)
 
     def test_logout(self):
