b9622f720daf73270ec8605751c2aaa1e5d71fe1
David Eriksson <the.dvudd@gmail.com>
2024-03-25 14:32:35 +0000

    refactor: refactor function for delete buttons

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Select att delete buttons
+  const deleteButtons = document.querySelectorAll('[data-delete-url]');
+  
+  // Attach a event listener to each button
+  deleteButtons.forEach(button => {
+      button.addEventListener('click', function() {
+          const deleteUrl = this.getAttribute('data-delete-url');
+          setDeleteUrl(deleteUrl);
+      });
+  });
+});
+
 function setDeleteUrl(url) {
-    document.getElementById('deleteConfirmBtn').href = url;
-  }
+  document.getElementById('deleteConfirmBtn').href = url;
+}
